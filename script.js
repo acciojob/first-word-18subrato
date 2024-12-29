fb4324fc753e9ee4b24d85374cf26a80e7115ce7
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2024-12-29 13:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
-  // your code here
+	s.trim();
+	s += ' ';
+	let word = '';
+	for(let t of s){
+		if(t !== ' '){
+			word += t;
+		} else {
+			return word;
+		}
+	}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:")
 alert(firstWord(s));
